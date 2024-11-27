b527b09f424dfb5428125be34fd7b6eeeda262bd
kmillanmx <102334878+kmillanmx@users.noreply.github.com>
2024-11-26 21:05:43 -0600

    Create Ej01_AreaCirculo.java

---
+++ 04_OperadoresAritméticos/Ej01_AreaCirculo.java
@@ -0,0 +1,7 @@
+public class AreaCirculo {
+    public static void main(String[] args) {
+        double radio = 7;  // radio del círculo
+        double area = Math.PI * Math.pow(radio, 2);  // área usando la fórmula
+        System.out.println("Área del círculo con radio " + radio + ": " + area);
+    }
+}
